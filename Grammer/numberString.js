386142ec27bb4e607cb053228391ca2f4e2e8978
Hosinging <pdhou23@gmail.com>
2023-04-17 01:53:20 +0900

    feat: 정수를 문자열로, 문자열을 정수로

---
+++ Grammer/numberString.js
@@ -0,0 +1,9 @@
+//Int > String
+let numberString = "777"
+let number = Number(numberString)
+console.log(number)
+
+//String > Int
+let number2 = 777
+let numberString2 = String(number2)
+console.log(numberString2)
