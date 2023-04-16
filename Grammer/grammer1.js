9c14a3636c8111281d60c86bbd1dfa9a713489fc
Hosinging <pdhou23@gmail.com>
2023-04-17 00:41:52 +0900

    feat: 사칙연산

--- Grammer/grammer1.js
+++ Grammer/grammer1.js
@@ -1,5 +1,19 @@
+// 문자열 출력
 console.log('Hello world!');
 
 result = 35;
 
 console.log(`정답은 ${result}입니다.`)
+
+// 사칙 연산
+a = 7
+b = 3
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(parseInt(a / b)) //몫
+console.log(a % b) //나머지
+
+//
