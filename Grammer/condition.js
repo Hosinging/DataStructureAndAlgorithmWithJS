09d6e6861814fcdcabdd918da7a3209c0411667b
Hosinging <pdhou23@gmail.com>
2023-04-17 01:50:06 +0900

    feat: condition, for, while

---
+++ Grammer/condition.js
@@ -0,0 +1,35 @@
+//조건문
+
+/*
+    if (condition)
+        statement1
+    else if (condition2)
+        statement2
+    ...
+    else
+        statementN
+*/
+
+
+//for문
+/*
+    for (초기문; 조건문; 증감문) {
+        //statements
+    }
+*/
+
+//1부터 100까지 합
+let sum = 0
+for (let i = 1; i <= 100; i++) {
+    sum += i
+}
+
+console.log(sum)
+
+//while
+//조건이 '참'일 때만 실행, 거짓인 경우 벗어남
+/*
+    while (조건문) {
+        //실행되는 코드
+    }
+*/
